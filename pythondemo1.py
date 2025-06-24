62d001bc911ad637f70e2c549b2ce41110eb5916
Stella <dancingpandasasa@gmail.com>
2025-06-24 14:45:39 +0800

    第三次修改

--- pythondemo1.py
+++ pythondemo1.py
@@ -7,6 +7,10 @@ mysql.connector.connect(
     port=3306,
     user="dbuser",
     password="1234"
+    database="world"
 
 )
 print("透過連線取得 cursor物件")
+dbcursor=cnx.cursor()
+print("執行 select name from city")
+dbcursor.execute("select name from city")
