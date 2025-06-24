4cddce76ebdf3f69455ecfa83717786716a49ac5
Stella <dancingpandasasa@gmail.com>
2025-06-24 14:15:10 +0800

    第二次commit

--- pythondemo1.py
+++ pythondemo1.py
@@ -1,2 +1,12 @@
-import
-mysql.connector
+import mysql.connector
+
+#
+print("建立資料庫連線中")
+mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="dbuser",
+    password="1234"
+
+)
+print("透過連線取得 cursor物件")
